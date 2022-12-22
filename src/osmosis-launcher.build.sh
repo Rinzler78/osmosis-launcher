61f971ec3082df16e867d693331ff8748250a414
Boris LECLERE <Borisleclere@gmail.com>
2022-12-22 17:23:56 +0100

    Clone osmosis source with specified tag => Version > v12.3.0 does not work Modify main.go file to integrate launcher args option Build osmosisd and test

---
+++ src/osmosis-launcher.build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+version="v12.3.0"
+
+if ! ./osmosis.clone.sh $version
+then
+    echo "Cannot build osmosis-launcher"
+    exit 1
+fi
+
+mainFilePath="osmosis/cmd/osmosisd/main.go"
+mainFileBaseDirectory=${mainFilePath%/*}
+
+if ! [[ -f $mainFilePath ]]
+then
+    echo "$mainFilePath not exists"
+    exit 1 
+fi
+
+mainGoFile="$(cat $mainFilePath)"
+launcherGoFilePath="launcher.go"
+targetLauncherGoFile="$mainFileBaseDirectory/$launcherGoFilePath"
+
+if [[ -f $targetLauncherGoFile ]]
+then
+    echo "Remove $targetLauncherGoFile"
+    rm -rf $targetLauncherGoFile
+fi
+
+# Copy launcher go file to cmd/osmosisd directory
+cp $launcherGoFilePath $targetLauncherGoFile
+
+## Insert wait for launcher function
+rootCmdLine="cmd.NewRootCmd()"
+waitForLauncherFunctionCall="wait_for_launcher()"
+newCodeLine="$rootCmdLine\n\t$waitForLauncherFunctionCall"
+mainGoFile="${mainGoFile/$rootCmdLine/$newCodeLine}"
+
+# Write new code
+echo -e "$mainGoFile" > $mainFilePath
+
+# Build osmosisd
+echo "Building osmosis"
+if make build -C osmosis
+then
+
+    if [[ -f $osmosisd-launcher ]]
+    then
+        echo "Removinf $osmosisd-launcher"
+        rm $osmosisd-launcher
+    fi
+
+    # Copy generated binary
+    cp osmosis/build/osmosisd osmosisd-launcher
+
+    # Show version
+    if ./osmosisd-launcher --launcher version
+    then
+        echo "It works !!"
+    else
+        echo "Seems does not work"
+        exit 1
+    fi
+else
+    echo "Build failed"
+    exit 1
+fi
+
+rm -rf osmosis
---
+++ src/osmosis.clone.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+gitHubRepoUrl="https://github.com/osmosis-labs/osmosis.git"
+version=$1
+targetFolder="osmosis"
+
+if [[ -z $version ]]
+then
+    echo "No version set."
+    version="main"
+fi
+
+if ! command -v git &> /dev/null
+then
+    echo "git not installed"
+    exit 1
+fi
+
+if [[ -d "$targetFolder" ]]
+then
+    echo "Removing $targetFolder"
+    rm -rf $targetFolder
+fi
+
+echo "Cloning tag $version @ $gitHubRepoUrl"
+if ! git clone $gitHubRepoUrl --branch $version --single-branch $targetFolder
+then
+    echo "git clone $gitHubRepoUrl --branch $version --single-branch $targetFolder clone failed"
+    exit 1
+fi
+
+
