b739cd3eb5992f64cac9db4499d43142784ae799
Boris LECLERE <Borisleclere@gmail.com>
2023-02-02 17:40:16 +0100

    Use uname in goland url

--- src/go.install.sh
+++ src/go.install.sh
@@ -7,7 +7,7 @@ then
     requiered_goland_version=$(./go.requieredVersion.sh $osmosis_version)
     echo "Need to install goland => $requiered_goland_version"
 
-    tar_file="$requiered_goland_version.darwin-amd64.tar.gz"
+    tar_file="$requiered_goland_version.$(echo "$(uname)" | tr '[:upper:]' '[:lower:]')-amd64.tar.gz"
     wget "https://go.dev/dl/$tar_file"
     tar -zxf "$tar_file"
     sudo rm -rf /usr/local/go ~/go ~/.go
