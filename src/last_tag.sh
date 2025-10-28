925a797a624244e78082612490a1d3397285d9a3
Boris LECLERE <Borisleclere@gmail.com>
2025-10-28 14:19:53 +0100

    Update last_tag.sh to exclude pre-release tags from the latest version output

--- src/last_tag.sh
+++ src/last_tag.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Print the latest tag (version) available from the Osmosis repo
+# Excludes pre-release tags (rc, alpha, beta, etc.)
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-"$SCRIPT_DIR/tags.sh" | tail -n 1 
+"$SCRIPT_DIR/tags.sh" | grep -v -E -- '-(rc|alpha|beta|test|mempool)' | tail -n 1 
